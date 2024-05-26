8a8543a6677d2176b476933cfa9d05b7d4951eba
Jason Adams <j.adams@f5.com>
2024-05-26 15:01:34 -0700

    Part 4 - Make DatePicker a TextView field to prevent user manual input

--- app/src/main/java/pw/jadams/mybicycleshopflamingo/ui/PartDetails.java
+++ app/src/main/java/pw/jadams/mybicycleshopflamingo/ui/PartDetails.java
@@ -3,21 +3,17 @@ package pw.jadams.mybicycleshopflamingo.ui;
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
 import android.app.PendingIntent;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -43,13 +39,14 @@ public class PartDetails extends AppCompatActivity {
     Repository repository;
     DatePickerDialog.OnDateSetListener startDate;
     final Calendar myCalendarStart = Calendar.getInstance();
+    final String myDateFormat = "MM/dd/yy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_details);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        repository=new Repository(getApplication());
+        repository = new Repository(getApplication());
         name = getIntent().getStringExtra("name");
         editName = findViewById(R.id.editPartTitle);
         editName.setText(name);
@@ -58,63 +55,51 @@ public class PartDetails extends AppCompatActivity {
         editPrice.setText(Double.toString(price));
         partId = getIntent().getIntExtra("id", -1);
         productId = getIntent().getIntExtra("productId", -1);
-        editNote=findViewById(R.id.editPartNote);
-        editDate=findViewById(R.id.editPartDate);
-        String myFormat = "MM/dd/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        editNote = findViewById(R.id.editPartNote);
+        editDate = findViewById(R.id.editPartDate);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myDateFormat, Locale.US);
 
         ArrayList<Product> productArrayList = new ArrayList<>(repository.getmAllProducts());
-        ArrayList<Integer> productIdList= new ArrayList<>();
-        for(Product product:productArrayList){
+        ArrayList<Integer> productIdList = new ArrayList<>();
+        for (Product product : productArrayList) {
             productIdList.add(product.getProductId());
         }
-        ArrayAdapter<Integer> productIdAdapter= new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item,productIdList);
-        Spinner spinner=findViewById(R.id.partSpinner);
+        ArrayAdapter<Integer> productIdAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, productIdList);
+        Spinner spinner = findViewById(R.id.partSpinner);
         spinner.setAdapter(productIdAdapter);
 
-        startDate = new DatePickerDialog.OnDateSetListener() {
+        startDate = (view, year, monthOfYear, dayOfMonth) -> {
+            myCalendarStart.set(Calendar.YEAR, year);
+            myCalendarStart.set(Calendar.MONTH, monthOfYear);
+            myCalendarStart.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-
-                myCalendarStart.set(Calendar.YEAR, year);
-                myCalendarStart.set(Calendar.MONTH, monthOfYear);
-                myCalendarStart.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-
-                updateLabelStart();
-            }
-
+            PartDetails.this.updateLabelStart();
         };
 
-        editDate.setOnClickListener(new View.OnClickListener() {
+        editDate.setOnClickListener(view -> {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Date date;
-                //get value from other screen,but I'm going to hard code it right now
-                String info=editDate.getText().toString();
-                if(info.equals(""))info="02/10/22";
-                try{
-                    myCalendarStart.setTime(sdf.parse(info));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                new DatePickerDialog(PartDetails.this, startDate, myCalendarStart
-                        .get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
-                        myCalendarStart.get(Calendar.DAY_OF_MONTH)).show();
+            //get value from other screen,but I'm going to hard code it right now
+            String currentDate = editDate.getText().toString();
+
+            if (currentDate.isEmpty())
+                currentDate = simpleDateFormat.format(Calendar.getInstance().getTime());
+            try {
+                myCalendarStart.setTime(simpleDateFormat.parse(currentDate));
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
             }
+            new DatePickerDialog(PartDetails.this, startDate, myCalendarStart
+                    .get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
+                    myCalendarStart.get(Calendar.DAY_OF_MONTH)).show();
+
         });
 
     }
-    private void updateLabelStart() {
-        String myFormat = "MM/dd/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-        editDate.setText(sdf.format(myCalendarStart.getTime()));
+    private void updateLabelStart() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myDateFormat, Locale.US);
+
+        editDate.setText(simpleDateFormat.format(myCalendarStart.getTime()));
     }
 
 
@@ -125,18 +110,15 @@ public class PartDetails extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId()== android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             this.finish();
-            return true;}
-        // return true;
-//                Intent intent=new Intent(PartDetails.this,MainActivity.class);
-//                startActivity(intent);
-//                return true;
+            return true;
+        }
 
-        if (item.getItemId()== R.id.save_part){
+        if (item.getItemId() == R.id.save_part) {
             Part part;
             if (partId == -1) {
-                if (repository.getmAllParts().size() == 0)
+                if (repository.getmAllParts().isEmpty())
                     partId = 1;
                 else
                     partId = repository.getmAllParts().get(repository.getmAllParts().size() - 1).getPartId() + 1;
@@ -148,8 +130,10 @@ public class PartDetails extends AppCompatActivity {
                 repository.update(part);
                 this.finish();
             }
-            return true;}
-        if (item.getItemId()== R.id.share_part) {
+            return true;
+        }
+
+        if (item.getItemId() == R.id.share_part) {
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_TEXT, editNote.getText().toString());
@@ -159,26 +143,27 @@ public class PartDetails extends AppCompatActivity {
             startActivity(shareIntent);
             return true;
         }
-        if(item.getItemId()== R.id.notify_part) {
+
+        if (item.getItemId() == R.id.notify_part) {
             String dateFromScreen = editDate.getText().toString();
-            String myFormat = "MM/dd/yy"; //In which you need put here
-            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-            Date myDate = null;
+            String myDateFormat = "MM/dd/yy"; //In which you need put here
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myDateFormat, Locale.US);
+            Date myDate;
             try {
-                myDate = sdf.parse(dateFromScreen);
+                myDate = simpleDateFormat.parse(dateFromScreen);
             } catch (ParseException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
-            try{
+            try {
                 assert myDate != null;
                 long trigger = myDate.getTime();
                 Intent intent = new Intent(PartDetails.this, MyReceiver.class);
                 intent.putExtra("key", "message I want to see");
                 PendingIntent sender = PendingIntent.getBroadcast(PartDetails.this, ++MainActivity.numAlert, intent, PendingIntent.FLAG_IMMUTABLE);
                 AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, trigger, sender);}
-            catch (Exception e){
-
+                alarmManager.set(AlarmManager.RTC_WAKEUP, trigger, sender);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
             return true;
         }
